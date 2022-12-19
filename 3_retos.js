687972dc8d22c9db4b53594751e1be273a2ad16a
Fabricio L <114324003+FabricioLM@users.noreply.github.com>
2022-12-19 15:01:24 -0300

    Create 3_retos.js

---
+++ 3_retos.js
@@ -0,0 +1,34 @@
+# Iterar-3-retos
+Retos de bucles.
+//Reto de codigo 1: que recorra un arreglo de números e imprima cada número.
+//var arr1 = [8, 6, 7, 5, 3, 0, 9];
+//for( var i = arr1.length -1 ; i>=0; i = i - 1){
+  // console.log(arr1[i]);
+//}
+
+
+
+//Reto de codigo 2:  que imprima la suma del número y el índice del número en el arreglo.
+//var arr2 = [4, 7, 13, 13, 19, 37, -2];
+//var suma = 0
+  //for(i = 0; i<arr2.length; i ++){
+    //    suma = suma + arr2[i];
+      //  console.log(i); 
+        //console.log(suma)
+  //}
+  
+
+
+
+
+//Reto de codigo 3: que solo imprima valores mayores a 5
+//var arr3 = [6, 2, 12, 14, -24, 5, 0];
+//for(var i = 0; i < arr3.length; i ++){
+    //if(arr3[i] > 5) {
+        //console.log(arr3[i])
+   // }
+     //(BONUS) 
+    // else {
+      //     [arr3[i]] = "No hay dados"
+     //}
+     //}
